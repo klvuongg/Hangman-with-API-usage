389a4bcf55b29d21da93c69184cd1e480d6f1490
klvuongg <160398807+klvuongg@users.noreply.github.com>
2024-12-04 16:01:22 -0500

    update new features

--- WordGuessing.py
+++ WordGuessing.py
@@ -22,9 +22,10 @@ word_bank.extend(fetch_words(api_url))
 if not word_bank:
     print("No words available from the API. Exiting...")
     exit()
+    
 
 word = random.choice(word_bank)
-guessedWord = [' ' if char == ' ' else '_' for char in word]
+guessedWord = [' ' if char == ' ' else '-' if char == '-' else '_' for char in word]
 attempts = 8
 guessedLetters = set()
 hang = ["_______",
@@ -66,22 +67,27 @@ def update_hangman(hang, man, wrong_attempts):
 
 while attempts > 0:
     print('\nCurrent word: ' + ' '. join(guessedWord))
-    guess = input('Guess a letter: ')
+    guess = input('Guess a letter or the whole word: ')
 
     if not guess.isalpha():
         print('Invalid input! Please guess an alphabetic character.')
         continue
-    if len(guess) > 1:
-        print('You can only guess a letter at a time!')
-        continue
+
     if guess in guessedLetters:
-        print(f'You have already guessed "{guess}". Try a different letter!')
+        print(f'You have already guessed "{guess}". Try a different letter/word!')
         continue
     guessedLetters.add(guess)
+    print(f'You have tried {guessedLetters}')
+
     if guess in word:
-        for i in range(len(word)):
-          if word[i] == guess:
-            guessedWord[i] = guess
+        if len(guess) > 1:
+          for i in range(len(word)):
+             if word[i] == guess[i]:
+               guessedWord[i] = guess[i]
+        elif len(guess) == 1:
+            for i in range(len(word)):
+             if word[i] == guess:
+               guessedWord[i] = guess
         print('Great guess!')
     else:
         update_hangman(hang, man, attempts)
@@ -91,12 +97,13 @@ while attempts > 0:
 
     if '_' not in guessedWord:
         print('\nCongratulations! You guessed the word: ' + word)
+        if len(guessedLetters) == 1:
+            print('\nYou guessed correctly in: 1 guess!')
+        elif len(guessedLetters) > 1:
+            print('\nYou guessed correctly in: ' + str(len(guessedLetters)) + ' guesses!')
         break
     elif attempts == 0:
         man[1] = "   (X.X)"  # Update face to dead
         hang[2] = "| " + man[1]  # Reflect change in hang structure
         print('\nYou\'ve run out of attempts! The word was: ' + word)
     print("\n".join(hang))
-
-
-    
